a7661d243b006cecac2b0071160df63d4bc2fd4a
m025863e <m025863e@staffs.ac.uk>
2015-01-22 10:45:54 +0000

    this aint gonna be here for long

---
+++ worthlessFile.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m025863e
+ * Date: 22/01/15
+ * Time: 10:43
+ */
+
+class worthlessFile {
+    public $dont;
+    public $care;
+
+    public function nothing($dont, $care){
+        $dont = "I don't even";
+        $care = "Care";
+        echo $dont . $care;
+    }
+} 
