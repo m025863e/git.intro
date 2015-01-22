08a82dd975d9b31e8c32662637b9c56038d56f52
m025863e <m025863e@staffs.ac.uk>
2015-01-22 10:38:03 +0000

    First file being added!

---
+++ gitTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: m025863e
+ * Date: 22/01/15
+ * Time: 10:35
+ */
+
+class gitTest {
+    public function testFunction{
+        $test = "";
+
+        echo $test;
+    }
+} 
